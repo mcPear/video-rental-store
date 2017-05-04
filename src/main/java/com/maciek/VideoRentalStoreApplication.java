2122757a46029ca866ee103c10a023b457ad2764
Maciej Gruszczyński <228131@student.pwr.edu.pl>
2017-05-04 21:17:04 +0200

    initial project version

---
+++ src/main/java/com/maciek/VideoRentalStoreApplication.java
@@ -0,0 +1,12 @@
+package com.maciek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maciek/VideoRentalStoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maciek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoRentalStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
