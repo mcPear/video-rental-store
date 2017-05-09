2f5fd3bea1829a1b1e387adeaa4a4a6e2b8b0217
mcPear <228131@student.pwr.edu.pl>
2017-05-09 20:28:04 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 mvn clean package
-
-if which xdg-open > /dev/null
-then
-  xdg-open http://localhost:8080
-elif which gnome-open > /dev/null
-then
-  gnome-open http://localhost:8080
-fi
